4c76213857f809510f2eb522b9744d54c6f52636
Enes Berk Yetim <110656328+enesberkyetim@users.noreply.github.com>
2025-03-19 14:44:44 +0300

    Add files via upload

--- mips_proje.asm
+++ mips_proje.asm
@@ -23,8 +23,15 @@ main :
 	jal number_counter # Calling number_counter function
 	
 	slti $t0, $v0, 7 # Checking whether the number count is below 7 or not
-	beq $t0, 1, count_error # If below 7 jump to count_error and exit
+	beq $t0, 1, print_error # If below 7 jump to count_error and exit
 	
+	jal uppercase_check
+	
+	beq $v0, 0, print_error
+	
+	jal lowercase_check
+	
+	beq $v0, 0, print_error
 	
 	li $v0, 4
 	la $a0, output
@@ -36,7 +43,7 @@ main :
 	
 	li $v0, 10 # All things are done in main function, so... quit
 	syscall
-count_error:
+print_error:
 	# Notify user by printing strings then exit
 	li $v0, 4
 	la $a0, output
@@ -55,7 +62,7 @@ number_counter:
 	li $t1, 0 # Will be the counter in number_counter
 	count_letters:	
 		lb $t0, 0($a0)
-		beq $t0, $zero, end_count
+		beq $t0, 10, end_count # Check if the current character is '\n' (break)
 		addi $t1, $t1, 1 # Increment counter +1
 		addi $a0, $a0, 1 # Increment a0 so that we can read the next character's address
 		j count_letters
@@ -64,6 +71,56 @@ number_counter:
 		addi $sp, $sp, 4
 		add $v0, $t1, $zero # Copying t1 to v0 for returning the total count
 		jr $ra
+		
+uppercase_check:
+	addi $sp, $sp, -4 # Store the a0 in stack as we will modify it in this procedure
+	sw $a0, 0($sp)
+	count_upper:
+		lb $t0, 0($a0) # Load the next character
+		li $t3, 64
+		slt $t1, $t3, $t0 # Is it over or equal 65 == 'A'
+		slti $t2, $t0, 91 # Is it below or equal 90 == 'Z'
+		and $t4, $t1, $t2 # Both should be satisfied
+		beq $t4, 1, end_upper # We found an uppercase letter so our job is done
+		beq $t0, 10, false_upper # If we come to '\n' that means there is no uppercase so, false
+		addi $a0, $a0, 1 # Will get the next character by incrementing a0 by 1
+		j count_upper
+	end_upper:
+		lw $a0, 0($sp) # Restore a0 which holds the address of the start of the input string
+		addi $sp, $sp, 4
+		li $v0, 1 # Return true in v0
+		jr $ra 
+	false_upper:
+		lw $a0, 0($sp) # Restore a0 which holds the address of the start of the input string
+		addi $sp, $sp, 4
+		li $v0, 0 # Return false
+		jr $ra
+		
+lowercase_check:
+	addi $sp, $sp, -4 # Store the a0 in stack as we will modify it in this procedure
+	sw $a0, 0($sp)
+	count_lower:
+		lb $t0, 0($a0) # Load the next character
+		li $t3, 64
+		slt $t1, $t3, $t0 # Is it over or equal 65 == 'A'
+		slti $t2, $t0, 91 # Is it below or equal 90 == 'Z'
+		and $t4, $t1, $t2 # Both should be satisfied
+		beq $t4, 1, end_lower # We found an lowercase letter so our job is done
+		beq $t0, 10, false_lower # If we come to '\n' that means there is no lowercase so, false
+		addi $a0, $a0, 1 # Will get the next character by incrementing a0 by 1
+		j count_lower
+	end_lower:
+		lw $a0, 0($sp) # Restore a0 which holds the address of the start of the input string
+		addi $sp, $sp, 4
+		li $v0, 1 # Return true in v0
+		jr $ra 
+	false_lower:
+		lw $a0, 0($sp) # Restore a0 which holds the address of the start of the input string
+		addi $sp, $sp, 4
+		li $v0, 0 # Return false
+		jr $ra
+		
+	
 	
 	
 	
